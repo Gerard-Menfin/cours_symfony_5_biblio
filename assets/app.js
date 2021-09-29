a82878dc2a0da55e563326436731cfe50e7f3bdb
Gerard-Menfin <gerard.mentor314@gmail.com>
2021-09-29 12:05:48 +0200

    CSS : fix hauteur 'html'

--- assets/app.js
+++ assets/app.js
@@ -16,3 +16,12 @@ global.$ = global.jquery = $;
 // import './bootstrap';
 require("bootstrap");
 
+
+$(function(){
+    console.log("%c chargement js 0.1", 'background: #222; color: #bada55');
+    
+    if( $("html").prop("clientHeight") < $("body > .container").prop("clientHeight") ) {
+        $("html").css("height", $("body>.container").prop("clientHeight"));
+    }
+});
+
